1d2d832277356cf29095f1d31bd857f1c26113df
Autulove <1657636021@qq.com>
2025-11-23 22:16:37 +0800

    vault backup: 2025-11-23 22:16:37

--- push-mkdocs.bat
+++ push-mkdocs.bat
@@ -1,9 +1,9 @@
 @REM @echo off
-cd /d "D:\VSCODE\rangoljx.github.io"
+cd /d "E:\4utumny.github.io"
 set commitDate=%date:~0,4%-%date:~5,2%-%date:~8,2%
 set /p commitMessage="Please enter the submit info:"
 git add .
 git commit -m "autocommit: %commitMessage% - %commitDate%"
 @REM git commit -m "auto-commit - %commitDate%"
-git push -u origin master
+git push -u origin main
 @REM git push -u origin master --force
